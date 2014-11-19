04217779c6c6d4802ba57b7d27b1777a843b208f
Roi Driscoll <roi.driscoll@gmail.com>
2014-11-19 10:35:41 +0000

    Added projectiles

--- game.rb
+++ game.rb
@@ -1,22 +1,28 @@
 #!/usr/bin/env ruby -w
 require 'gosu'
 require './lib/player.rb'
+require './lib/projectile.rb'
+
 
 class GameWindow < Gosu::Window
   def initialize
     super(640, 480, false)
     @background_image = Gosu::Image.new(self, "assets/background.png", true)
     @player = Player.new(self) # Self refers to the window
+    @projectiles = []
   end
 
   def draw
     @background_image.draw(0,0,0)
     @player.draw
+    @projectiles.each { |projectile| projectile.draw }
   end
 
   def update
     @player.move
     control_player
+    @projectiles.each    { |projectile| projectile.move  }
+    @projectiles.reject! { |projectile| projectile.dead? }
   end
 
   def control_player
@@ -33,6 +39,9 @@ class GameWindow < Gosu::Window
 
   def button_down(id)
     close if id == Gosu::KbQ # Creates'quit' function
+    if id == Gosu::KbSpace
+      @projectiles << Projectile.new(self, @player)
+    end
   end
 
 end
