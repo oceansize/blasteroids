6439b953dc81fdd3651e122fa898c4a14b0b5455
Roi Driscoll <roi.driscoll@gmail.com>
2014-11-19 10:20:23 +0000

    Added ship, movement and quit function

--- game.rb
+++ game.rb
@@ -1,14 +1,38 @@
 #!/usr/bin/env ruby -w
 require 'gosu'
+require './lib/player.rb'
 
 class GameWindow < Gosu::Window
   def initialize
     super(640, 480, false)
     @background_image = Gosu::Image.new(self, "assets/background.png", true)
+    @player = Player.new(self) # Self refers to the window
   end
 
   def draw
     @background_image.draw(0,0,0)
+    @player.draw
+  end
+
+  def update
+    @player.move
+    control_player
+  end
+
+  def control_player
+    if button_down? Gosu::KbLeft
+      @player.turn_left
+    end
+    if button_down? Gosu::KbRight
+      @player.turn_right
+    end
+    if button_down? Gosu::KbUp
+      @player.accelerate
+    end
+  end
+
+  def button_down(id)
+    close if id == Gosu::KbQ # Creates'quit' function
   end
 
 end
