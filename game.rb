99b699aeff1b8c34fe6ef48d2b953112ad93c9d9
Roi Driscoll <roi.driscoll@gmail.com>
2014-11-19 10:49:28 +0000

    Added asteroids

--- game.rb
+++ game.rb
@@ -2,6 +2,7 @@
 require 'gosu'
 require './lib/player.rb'
 require './lib/projectile.rb'
+require './lib/asteroid.rb'
 
 
 class GameWindow < Gosu::Window
@@ -10,17 +11,22 @@ class GameWindow < Gosu::Window
     @background_image = Gosu::Image.new(self, "assets/background.png", true)
     @player = Player.new(self) # Self refers to the window
     @projectiles = []
+    @asteroids = Asteroid.spawn(self, 3)
   end
 
   def draw
     @background_image.draw(0,0,0)
     @player.draw
     @projectiles.each { |projectile| projectile.draw }
+    @asteroids.each   { |asteroid| asteroid.draw     }
   end
 
   def update
     @player.move
     control_player
+    @asteroids.each      { |asteroid| asteroid.move  }
+    @asteroids.reject!   { |asteroid| asteroid.dead? }
+    detect_collisions
     @projectiles.each    { |projectile| projectile.move  }
     @projectiles.reject! { |projectile| projectile.dead? }
   end
@@ -42,6 +48,29 @@ class GameWindow < Gosu::Window
     if id == Gosu::KbSpace
       @projectiles << Projectile.new(self, @player)
     end
+  end
+
+  def detect_collisions
+    @asteroids.each do |asteroid|
+      if collision?(asteroid, @player)
+        puts 'kaboom'
+      end
+    end
+    @projectiles.each do |projectile|
+      @asteroids.each do |asteroid|
+        if collision?(projectile, asteroid)
+          projectile.kill
+          asteroid.kill
+        end
+      end
+    end
+  end
+
+  def collision?(object_1, object_2)
+    hitbox_1, hitbox_2 = object_1.hitbox, object_2.hitbox
+    common_x = hitbox_1[:x] & hitbox_2[:x]
+    common_y = hitbox_1[:y] & hitbox_2[:y]
+    common_x.size > 0 && common_y.size > 0
   end
 
 end
